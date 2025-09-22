ac055eeb32bec6487e5b34712ff02c633abb35a4
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-09-22 23:18:37 +0200

    feat (Web): Adding Ui shadcdn

--- frontend/web/app/page.tsx
+++ frontend/web/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
   <>
     <h1>
       Hello, Area!
     </h1>
+    <Button>Click me</Button>
   </>
   );
 }
