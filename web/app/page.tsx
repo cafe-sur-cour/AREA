a418fd75706971c3c3cc110c9006194754e374c9
Albane Merian <albane.merian@epitech.eu>
2025-09-23 10:51:03 +0200

    feat (Lint): Add the linter and prettier dependencies in the .json

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-  <>
-    <h1 className="text-3xl font-heading">
-      Hello, Area!
-    </h1>
-    <Button className="text-xl font-sans hover:underline">Click me</Button>
-  </>
+    <>
+      <h1 className='text-3xl font-heading'>Hello, Area!</h1>
+      <Button className='text-xl font-sans hover:underline'>Click me</Button>
+    </>
   );
 }
