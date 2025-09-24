3208fe6343ec5155b75a8cc4c7fe9451bbe9a6d5
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-09-24 11:39:15 +0200

    feat (navigation): add Navigation component and integrate into Home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@/components/ui/button';
+import Navigation from "@/components/header";
 
 export default function Home() {
   return (
     <>
+      <Navigation />
       <h1 className='text-3xl font-heading'>Hello, Area!</h1>
       <Button className='text-xl font-sans hover:underline'>Click me</Button>
     </>
