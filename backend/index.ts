626f0e88eb2b7b1ea02000912ffe4dc6b10c679a
Albane Merian <albane.merian@epitech.eu>
2025-09-24 14:26:15 +0200

    feat (API): Add the declaation of the api routes in the entrypoint file of the app

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,28 @@ import * as dotenv from 'dotenv';
 import { AppDataSource } from './src/config/db';
 import { saveData } from './src/app';
 import express from 'express';
+import crypto from "crypto";
+import cookieParser from "cookie-parser";
 import { waitForPostgres } from './src/utils/waitForDb';
 
+import authRoutes from './src/routes/auth/auth';
+import userRoutes from './src/routes/user/user';
+
 const app = express();
+export const JWT_SECRET = crypto.randomBytes(64).toString("hex");
 dotenv.config();
+
+/* Declare  the cors and allowed routes */
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+/* Route definition with API as prefix */
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 (async function start() {
   try {
@@ -15,7 +33,6 @@ dotenv.config();
     await AppDataSource.initialize(); /* Example initilization of elem in table */
 
     app.listen(3000, () => {
-      // Temp not define in .env
       console.log('Server is running on port 3000');
     });
 
