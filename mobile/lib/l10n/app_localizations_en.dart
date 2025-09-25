03c9dd1b0ccfe3f94c25fe5057fa1a9f192ad019
albanrss <alban.roussee@epitech.eu>
2025-09-25 18:05:14 +0200

    feat (l10n): added the translation packages, and created first translations for the nav bar

---
+++ mobile/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get label_home => 'Home';
+
+  @override
+  String get label_catalogue => 'Catalogue';
+
+  @override
+  String get label_add => 'Add';
+
+  @override
+  String get label_areas => 'AREAs';
+
+  @override
+  String get label_profile => 'Profile';
+}
