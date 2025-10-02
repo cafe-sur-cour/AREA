e1ba42a68c0de8f5893a38edde1c24b0e9908567
albanrss <alban.roussee@epitech.eu>
2025-10-02 11:52:09 +0200

    feat (navigation): enhance MainNavigation to support selected action, service, and reaction

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:area/l10n/app_localizations.dart';
+import 'package:area/screens/action_services_screen.dart';
+import 'package:area/screens/reaction_services_screen.dart';
 import 'package:area/screens/forgot_password_screen.dart';
 import 'package:area/screens/register_screen.dart';
 import 'package:area/screens/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
-import 'navigation/main_navigation.dart';
+import 'package:area/navigation/main_navigation.dart';
 import 'package:area/core/themes/app_theme.dart';
 import 'package:area/core/notifiers/backend_address_notifier.dart';
 import 'package:area/core/notifiers/locale_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -44,10 +46,20 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       initialRoute: '/',
       routes: {
-        '/': (context) => MainNavigation(),
+        '/': (context) {
+          final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+          return MainNavigation(
+            selectedAction: args?['selectedAction'],
+            selectedService: args?['selectedService'],
+            selectedReaction: args?['selectedReaction'],
+            selectedReactionService: args?['selectedReactionService'],
+          );
+        },
         '/login': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen(),
         '/forgot-password': (context) => const ForgotPasswordScreen(),
+        '/action-services': (context) => const ActionServicesScreen(),
+        '/reaction-services': (context) => const ReactionServicesScreen(),
       },
     );
   }
