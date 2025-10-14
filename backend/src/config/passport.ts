b1080433c29f51b73118f10c552e8d9e5fa2265c
Matisse-M <matisse.marsac@epitech.eu>
2025-10-14 16:25:38 +0200

    feat (backend): Implement create new calendar reaction

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -266,6 +266,7 @@ const googleScopes = [
   'https://www.googleapis.com/auth/userinfo.email',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/gmail.send',
+  'https://www.googleapis.com/auth/calendar.events',
 ];
 
 passport.use(
