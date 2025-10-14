d47caf0408de3e0b6564c249e7f026a8dfa3710b
eliotttesnier <eliott.tesnier@epitech.eu>
2025-10-14 09:15:59 +0200

    fix (backend): not unique token

--- database/table/user_token.sql
+++ database/table/user_token.sql
@@ -3,7 +3,7 @@ CREATE TABLE user_tokens (
     "id" SERIAL PRIMARY KEY,
     "user_id" INTEGER NOT NULL,
     "token_type" VARCHAR(50) NOT NULL,
-    "token_value" TEXT UNIQUE NOT NULL,
+    "token_value" TEXT NOT NULL,
     "expires_at" TIMESTAMP,
     "scopes" TEXT[],
     "is_revoked" BOOLEAN DEFAULT FALSE,
