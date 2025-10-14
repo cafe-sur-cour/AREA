8267923d8d820c8ec07d0f807a8ab2e5864da47a
eliotttesnier <eliott.tesnier@epitech.eu>
2025-10-14 10:56:30 +0200

    fix (backend): unique token

--- backend/src/config/entity/UserToken.ts
+++ backend/src/config/entity/UserToken.ts
@@ -17,7 +17,7 @@ export class UserToken extends BaseEntity {
   @Column({ type: 'varchar', length: 50 })
   token_type!: string;
 
-  @Column({ type: 'text' })
+  @Column({ type: 'text', unique: true })
   token_value!: string;
 
   @Column({ type: 'timestamp', nullable: true })
