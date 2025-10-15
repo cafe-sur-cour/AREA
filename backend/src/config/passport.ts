d0a6108e72dd970f33dfdc28cd3ce5da35f9fd96
Matisse-M <matisse.marsac@epitech.eu>
2025-10-15 09:26:23 +0200

    refactor (backend): scope for google agendas

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -266,7 +266,6 @@ const googleScopes = [
   'https://www.googleapis.com/auth/userinfo.email',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/gmail.send',
-  'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events'
 ];
 
