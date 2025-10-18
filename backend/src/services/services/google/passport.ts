bc7ba82adda7f68c0f3180b23b00c35bd7c98574
Matisse-M <matisse.marsac@epitech.eu>
2025-10-18 10:27:08 +0200

    refactor (backend): fix scopes in google's passport.ts

--- backend/src/services/services/google/passport.ts
+++ backend/src/services/services/google/passport.ts
@@ -25,8 +25,10 @@ const googleScopes = [
   'https://www.googleapis.com/auth/userinfo.email',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/gmail.send',
+  'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events',
   'https://www.googleapis.com/auth/documents',
+  'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/drive',
 ];
 
