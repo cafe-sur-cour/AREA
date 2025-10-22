ba14ba2d1b82267a2d864f2547cec4888cc0e9b0
Albane Merian <albane.merian@epitech.eu>
2025-10-22 16:49:30 +0200

    feat (Web): add gitlab connect button on the login page

--- web/components/login-form.tsx
+++ web/components/login-form.tsx
@@ -13,6 +13,7 @@ import { FaGithub, FaGoogle, FaMicrosoft, FaFacebook } from 'react-icons/fa';
 import { getAPIUrl } from '@/lib/config';
 import InputPassword from './ui/input-password';
 import { toast } from 'sonner';
+import { SiGitlab } from 'react-icons/si';
 
 export function LoginForm({
   className,
@@ -61,6 +62,10 @@ export function LoginForm({
 
   const signInWithMeta = async () => {
     window.location.href = `${await getAPIUrl()}/auth/facebook/login`;
+  };
+
+  const signInWithGitlab = async () => {
+    window.location.href = `${await getAPIUrl()}/auth/gitlab/login`;
   };
 
   return (
@@ -137,6 +142,13 @@ export function LoginForm({
                 >
                   <FaFacebook />
                   <span className='sr-only'>Login with Meta</span>
+                </ButtonWithLoading>
+                <ButtonWithLoading
+                  onClick={async () => await signInWithGitlab()}
+                  className='w-full cursor-pointer'
+                >
+                  <SiGitlab />
+                  <span className='sr-only'>Login with GitLab</span>
                 </ButtonWithLoading>
               </div>
               <div className='text-center text-sm'>
