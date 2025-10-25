716a2717062a22d4ed9620295d31167dede62231
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-10-25 15:29:02 +0200

    feat (Jest): Enhance Jest configuration and add setup file for testing environment

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -6,10 +6,31 @@ const createJestConfig = nextJest({
 });
 
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^@hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@contexts/(.*)$': '<rootDir>/contexts/$1',
+    '^@types/(.*)$': '<rootDir>/types/$1'
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
 };
 
 export default createJestConfig(config);
