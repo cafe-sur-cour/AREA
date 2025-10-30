de0a013bec9c76eb5f9e17acc3abf19405c5ab79
albanrss <alban.roussee@epitech.eu>
2025-10-30 14:49:38 +0100

    feat (app): add route for about screen

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -9,6 +9,7 @@ import 'package:area/screens/register_screen.dart';
 import 'package:area/screens/login_screen.dart';
 import 'package:area/screens/automation_configuration_screen.dart';
 import 'package:area/screens/dashboard_screen.dart';
+import 'package:area/screens/about_screen.dart';
 import 'package:area/navigation/main_navigation.dart';
 import 'package:area/core/themes/app_theme.dart';
 import 'package:area/core/notifiers/backend_address_notifier.dart';
@@ -94,6 +95,7 @@ class MyApp extends StatelessWidget {
         '/reaction-services': (context) => const ReactionServicesScreen(),
         '/automation-configuration': (context) => const AutomationConfigurationScreen(),
         '/dashboard': (context) => const DashboardScreen(),
+        '/about': (context) => const AboutScreen(),
       },
     );
   }
